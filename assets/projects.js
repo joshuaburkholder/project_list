c80ee2ea73ffc67e80ef38d9f495922b00d7b3d8
DESKTOP-BGOGHIS\BurkLesmeisterholder <joshburkholder31@gmail.com>
2017-08-02 01:58:38 -0400

    updated projectjs

--- assets/projects.js
+++ assets/projects.js
@@ -5,12 +5,28 @@
 const projects = [
 
   {
-    "title":"NAME OF APP",
-    "tags":"featured javascript jquery angularjs reactjs mongodb firebase css html",
-    "description":"DESCRIBE TECH AND USES",
-    "github":"http://",
-    "link":"https:///",
-    "img":"favicon.ico"
+    "title":"naptown.art",
+    "tags":"featured javascript firebase css html",
+    "description":"Utilizes JavaScript and LeafletJS to create a map of public art installations in Fountain Square (Indy) by using JavaScript to call and plot data points from Firebase (JSON) endpoint.",
+    // "github":"http://",
+    "link":"https://naptown.art/",
+    "img":"https://joshuaburkholder.github.io/assets/images/pf-naptownart.jpg"
+  },  
+    {
+    "title":"Media Mail Calculator",
+    "tags":"featured jquery javascript firebase css html",
+    "description":"Uses JavaScript & jQuery to validate user input. Uses JavaScript to calculate the cost of shipping via media mail (based on weight) and jQuery to manipulate the DOM and display results. Hosted on Firebase.",
+    // "github":"http://",
+    "link":"https://mediamail.info/",
+    "img":"https://joshuaburkholder.github.io/assets/images/pf-mediamail.jpg"
+  },  
+    {
+    "title":"eXamen",
+    "tags":"featured javascript reactjs mongodb css html node",
+    "description":"Built with ReactJS, this app allows authenticated users to sign in and log a daily reflection. Daily logs are stored in a Mongo (NoSQL) database using Express middleware and a NodeJS server.",
+    "github":"https://github.com/joshuaburkholder/MERN_project",
+    "link": "https://dailyexamenweb.herokuapp.com/",
+    "img":"https://joshuaburkholder.github.io/assets/images/examen1.jpeg"
   },  
   {
     "title":"",
